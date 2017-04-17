404db0f5eb62518721d9f76999b111fa89460e10
A. Cynic <chris@mretc.net>
2017-04-16 21:22:20 -0600

    Implement caching wrapper around requests.get

---
+++ feedfetch/cache_get.py
@@ -0,0 +1,129 @@
+"""
+This module provides a function, `cache_get`  which wraps the :meth:`get()`
+method from the `requests package`_ and provides persistent (thread and
+multiprocess-safe) caching to disk.
+
+It transparently handles retrieving resources from the cache, validating them,
+re-fetching stale resources including using etag and last-modified headers, and
+updating cached resources.
+
+Resources (even when stale) are never deleted from the cache. Pruning must be
+done separately, if needed.
+
+Usage is as simple as initializing a ShelfCache object with a path where the
+database file should be created, and then calling the `cache_get` function with
+the URL to fetch::
+
+    >>> from feedfetch.cache_get import cache_get
+    >>> from feedfetch import ShelfCache
+    >>> cache = FeedCache('path/to/cache.db')
+    >>> response = cache_get(cache, url='https://hnrss.org/newest')
+    >>> response.status_code
+    200
+    >>> len(response.text)
+    18236
+
+See the requests package documentation for how to use the response object:
+http://docs.python-requests.org/en/master/user/quickstart/#response-content
+
+.. _requests package: http://docs.python-requests.org/en/master/r
+"""
+import requests
+import re
+import logging
+from .shelfcache import ShelfCache
+from typing import Callable
+
+NOT_MODIFIED = 304
+
+logger = logging.getLogger(__name__)
+
+
+def cache_get(cache: ShelfCache, url: str, headers={},
+              get_meth: Callable=requests.get, **kwargs) -> requests.Response:
+    """
+    A wrapper around `requests.get()` which uses an on-disk cache.
+
+    Note that `url` must include all of its query sring fields (there is not
+    `params` parameter like with `requests.get()`
+
+    If the url is in the cache and it is still fresh then it is returned
+    directly. Items are valid for `cache.exp_seconds` or for the max-age
+    specified in the cache-control header returned by the server, whichever
+    is less.
+
+    If the item in the cache has expired, it is re-fetched from the remote
+    server (using etag and/or last-modified headers if available so that the
+    server can return a cached version).
+
+    When the response is received from the server, then the feed is updated
+    in the on-disk cache.
+
+    :param cache: The ShelfCache to handle the cache
+    :param url: the url of the resource to fetch
+    :param get_meth: The method which is called to issue the HTTP get request.
+        By default this is requests.get but is injectable mostly for testing
+        purposes.
+    :pram **kwargs: All keyword args are passed to `requests.get()`
+
+    Returns:
+        The requested resource. The requests package will raise exceptions
+        on network errors.
+    """
+    etag = None
+    lastmod = None
+
+    logger.info("Fetching item for url: {}".format(url))
+    item = cache.get(url)
+    if item:
+        logger.info("Got resource from cache for url: {}".format(url))
+        cached, expired = item.data, item.expired
+        if not expired:
+            # If cache is fresh, use it without further ado
+            logger.info("Returning fresh item found in cache: {}"
+                        .format(url))
+            return cached
+
+        logger.info("Stale item found in cache: {}".format(url))
+        etag = cached.headers.get('etag')
+        etag = etag.lstrip('W/') if etag else None  # strip weak etag
+        lastmod = cached.headers.get('last-modified')
+    else:
+        logger.info("No item in cache for url: {}".format(url))
+
+    # Cache wasn't fresh in db, so we'll request it, but give origin etag
+    # and/or last-modified headers (if available) so we only fetch and
+    # parse it if it is new/updated.
+    logger.info("Fetching from remote {}".format(url))
+
+    # Add if-none-match and/or if-modified-since headers
+    if etag:
+        headers['If-None-Match'] = etag
+    if lastmod:
+        headers['If-Modified-Since'] = lastmod
+
+    fetched = get_meth(url, headers=headers, **kwargs)
+    fetched.raise_for_status()
+    logger.info("Got resource from remote for  {}".format(url))
+    logger.debug("Resource: {}".format(fetched))
+
+    if fetched.status_code == NOT_MODIFIED:
+        # Source says feed is still fresh
+        logger.info("Server says resource is still fresh: {}".format(url))
+        new_headers = fetched.headers
+        fetched = cached
+        fetched.headers = new_headers
+
+    # Add to/update cache with new expire_dt
+    # Using max-age parsed from cache-control header, if it exists
+    cc_header = fetched.headers.get('cache-control') or ''
+    ma_match = re.search('max-age=(\d+)', cc_header)
+    if ma_match:
+        if cache.exp_seconds < 0:
+            min_age = int(ma_match.group(1))
+        else:
+            min_age = min(int(ma_match.group(1)), cache.exp_seconds)
+    else:
+        min_age = cache.exp_seconds
+    cache.create_or_update(url, data=fetched, exp_seconds=min_age)
+    return fetched
---
+++ test/test_cache_get.py
@@ -0,0 +1,162 @@
+import requests
+import requests.exceptions
+from feedfetch.cache_get import cache_get
+import unittest
+from unittest.mock import MagicMock
+from feedfetch.shelfcache import CacheResult
+
+NOT_MODIFIED = 304
+OK = 200
+
+ATOM_PATH = 'test/test_atom.xml'
+
+
+def mock_shelfcache(return_value=None):
+    """Helper function to create a mocked ShelfCache instance.
+
+    Args:
+        return_value: the value returned by the mocked shelf.get() method
+    Returns:
+        a MagicMock object spec'd to RWShelf
+    """
+    mock_shelf = MagicMock()
+    mock_shelf.exp_seconds = -1
+    mock_get = MagicMock(return_value=return_value)
+    mock_shelf.get = mock_get
+    return mock_shelf
+
+
+def build_response(contents="test", status=OK,
+                   etag='etag', modified='modified', max_age=None):
+    """Make a requests.Response object suitable for testing.
+
+    Args:
+        contents: the contents of the requested resource
+        status: HTTP status
+        exp-time: cache expire time (set to future for fresh cache, past for
+            stale cache (defaults to stale))
+        etag: etag cache-control header
+        modified: last-modified cache-control header
+
+    Returns:
+        A Response instance populated according to the arguments.
+    """
+    headers = {'last-modified': modified, 'etag': etag, 'Cache-Control':
+               'max-age={}'.format(max_age)}
+    test_response = requests.Response()
+    test_response.status_code = status
+    test_response.headers = headers
+    return test_response
+
+
+def build_getter(return_value):
+    """Build a mock requests.get method.
+
+    Args:
+        return_value: the value to be returned by the call to `get()`
+        (probably a requests.Response object.
+
+    Returns:
+        A MagicMock instance spec'd to feedparser.parse
+    """
+    mock_getter = MagicMock(spec=requests.get)
+    mock_getter.return_value = return_value
+    return mock_getter
+
+
+class TestCacheGet(unittest.TestCase):
+
+    def test_new(self):
+        """Simulate a URL not in the cache, and verify that cache_get tries to
+        fetch it over http."""
+        # setup mock locked_shelf:
+        new = build_response()
+        mock_shelf = mock_shelfcache(None)
+
+        # setup mock requests.get method
+        mock_getter = build_getter(new)
+
+        # DUT:
+        resp = cache_get(mock_shelf, url='fake_url', get_meth=mock_getter)
+
+        self.assertEqual(resp, new)
+        mock_getter.assert_called_once_with('fake_url', headers={})
+
+    def test_fresh(self):
+        """Simulate a freshly cached feed and verify that cache_get returns
+        it."""
+        # setup mock locked_shelf:
+        fresh = build_response()
+        mock_shelf = mock_shelfcache(CacheResult(data=fresh,
+                                                 expired=False))
+
+        # setup mock feedparser.parse method
+        mock_getter = build_getter(fresh)
+
+        # DUT:
+        resp = cache_get(mock_shelf, url='fake_url', get_meth=mock_getter)
+
+        self.assertEqual(resp, fresh)
+        # since feed is resh, assert that the parser is not called:
+        mock_getter.assert_not_called()
+
+    def test_stale_not_modified(self):
+        """Simulate a stale cached item and verify that cache_get fetches it and
+        then asks the remote server for an updated."""
+        # setup mocked RWShelf
+        stale = build_response(status=NOT_MODIFIED)
+        mock_shelf = mock_shelfcache(CacheResult(data=stale,
+                                                 expired=True))
+
+        # setup mock feedparser.parse method
+        mock_getter = build_getter(stale)
+
+        # DUT:
+        resp = cache_get(mock_shelf, url='fake_url', get_meth=mock_getter)
+
+        h = {'If-None-Match': 'etag', 'If-Modified-Since': 'modified'}
+        mock_getter.assert_called_once_with('fake_url', headers=h)
+        self.assertEqual(resp, stale)
+
+    def test_stale_modified(self):
+        """Simulate a stale cached item and verify that cache_get fetches it and
+        then updates with new feed from server."""
+        # setup mocked RWShelf
+        stale = build_response(status=OK)
+        mock_shelf = mock_shelfcache(CacheResult(data=stale,
+                                                 expired=True))
+
+        # setup mock feedparser.parse method  (and mock headers so we can verify
+        # that FeedCache parsed the cache-control header)
+        mock_headers = MagicMock(spec=dict)
+        mock_headers.get.return_value = 'max-age=10'
+        new = build_response(status=OK, contents="changed content")
+        new.headers = mock_headers
+        mock_getter = build_getter(new)
+
+        # instantiate DUT:
+        resp = cache_get(mock_shelf, url='fake_url', get_meth=mock_getter)
+
+        h = {'If-None-Match': 'etag', 'If-Modified-Since': 'modified'}
+        mock_getter.assert_called_once_with('fake_url', headers=h)
+        mock_headers.get.assert_called_with('cache-control')
+
+        # Make sure feed was updated:
+        mock_shelf.create_or_update.assert_called_with('fake_url',
+                                                       data=new,
+                                                       exp_seconds=10)
+        self.assertEqual(resp, new)
+
+    def test_404(self):
+        """Simulate fetching a non-existent resource."""
+        # setup mocked RWShelf
+        feed404 = build_response(status=404)
+        mock_shelf = mock_shelfcache(None)
+
+        # setup mock feedparser.parse method
+        mock_getter = build_getter(feed404)
+
+        with self.assertRaises(requests.exceptions.HTTPError) as e:
+            cache_get(mock_shelf, url='http://notfound/', get_meth=mock_getter)
+        self.assertEqual(404, e.exception.response.status_code)
+        mock_getter.assert_called_once_with('http://notfound/', headers={})
